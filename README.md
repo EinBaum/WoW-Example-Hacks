6585b474189b23fcd2e956046a0054d6a8a4d0e8
EinBaum <mail@einbaum.org>
2017-12-24 00:11:32 +0100

    New: SkinUnlocker

--- README.md
+++ README.md
@@ -6,8 +6,10 @@ AxisZ: Teleport hack that can only change your Z axis (height).
 
 FoV: Field of view fix that lets you see EVERYTHING.
 
-StopFall: Stops fall when you press a hotkey that you define.
-
 Inject: Useless test program.
 
-InjectLua: Injects code into WoW that prints Hello World in chat.
+InjectLua: Injects Lua code into WoW that prints Hello World in chat.
+
+StopFall: Stops player fall when you press a hotkey that you define.
+
+SkinUnlocker: Lets you select extra skin colors when making a new char.
---
+++ SkinUnlocker.c
@@ -0,0 +1,46 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdint.h>
+#include "lib/MemMgr.h"
+#include "lib/Misc.h"
+
+VOID DoInject(HWND hWnd)
+{
+	HANDLE hProcess = NULL;
+	DWORD processId = 0;
+
+	GetWindowThreadProcessId(hWnd, &processId);
+	hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
+
+	if (!hProcess)
+	{
+		printf("Cannot open process: Error %d...\n", GetLastError());
+		return FALSE;
+	}
+
+	/* Normally there is a function call that returns the maximum allowed
+	 * skin ID. Return value is placed into EAX. Replace function call with a
+	 * static value. */
+
+	/* Replaces:	00470FA9   E8 92940000      CALL WoW.0047A440	*/
+	/* With:		00470FA9   B8 0000002F      MOV EAX,2F000000	*/
+
+	UINT8 mov = 0xB8;			// MOV EAX
+	UINT32 val = 0x0000001F;	// 0x0000002F
+	MemMgr_Write(hProcess, 0x470FA9, &mov, 1);
+	MemMgr_Write(hProcess, 0x470FAA, &val, 4);
+
+	printf("Success.\n");
+	CloseHandle(hProcess);
+}
+
+int main(int argc, char* argv[])
+{
+	if (!Misc_EnableDebugPrivilege(TRUE))
+	{
+		printf("Run this program as Administrator.\n");
+		return 1;
+	}
+	Misc_FindWoW((Misc_FindWoW_Callback)DoInject);
+	return 0;
+}
