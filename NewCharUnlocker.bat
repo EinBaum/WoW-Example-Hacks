5f41ce6c228a0a67937c0d8fdea73ada01cd3cce
EinBaum <mail@einbaum.org>
2018-10-23 11:45:53 +0200

    fix filename in NewCharUnlocker build file

--- NewCharUnlocker.bat
+++ NewCharUnlocker.bat
@@ -1,1 +1,1 @@
-tcc -o bin/SkinUnlocker.exe lib/MemMgr.c lib/Misc.c lib/Inject.c lib/ObjMgr.c SkinUnlocker.c -luser32 -ladvapi32
+tcc -o bin/NewCharUnlocker.exe lib/MemMgr.c lib/Misc.c lib/Inject.c lib/ObjMgr.c NewCharUnlocker.c -luser32 -ladvapi32
